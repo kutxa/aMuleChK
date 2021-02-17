0d889429652f10d211af7aade5b02e3554921d36
Kutxa <79199197+kutxa@users.noreply.github.com>
2021-02-17 12:49:40 +0100

    Update temporizador.py

--- temporizador.py
+++ temporizador.py
@@ -3,11 +3,11 @@ import time
 import requests
 starttime=time.time()
 messages=['Codigo']
-id='1657793959'
+id=''
 for message in messages:
 	localtime = time.asctime( time.localtime(time.time()) )
 	mensaje = "Hora :" + localtime + "--" + message
-	r=requests.post('https://api.telegram.org/bot1460282614:AAF9GPKkPvhp0BD88BwuaMk_moYQbff9Zg4/sendMessage',
+	r=requests.post('https://api.telegram.org/bot<TOKEN>/sendMessage',
               data={'chat_id': id, 'text': mensaje})
 	print(r.text)	
 	time.sleep(10.0 - ((time.time() - starttime) % 10.0))

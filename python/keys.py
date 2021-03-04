a341c6aae23a3decb9f8fd90a787f1ad045424e9
Kutxa <79199197+kutxa@users.noreply.github.com>
2021-03-04 11:38:00 +0100

    Structure of the keys file.

---
+++ python/keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+######### USER VARIABLES ###############
+# path 
+user_path = '/usr/bin/amulecmd'
+token = '1460282614:AAF9GPKkPvhp0BD88BwuaMk_moYQbff9Zg4'
+user_id = 1657793959
+user_txt = 'kutxa'
+aMulepass = 'k69aMuleWeb'
+aMulelogo = 'aMuleBNfondo.png'
+avatar = 'avatarBNfondo.png'
+

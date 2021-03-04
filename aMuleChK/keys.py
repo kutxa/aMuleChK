a4557550fae653844ed8903e137a10de9c38309b
Kutxa <79199197+kutxa@users.noreply.github.com>
2021-03-04 11:45:06 +0100

    clear keys

--- aMuleChK/keys.py
+++ aMuleChK/keys.py
@@ -3,10 +3,9 @@
 ######### USER VARIABLES ###############
 # path 
 user_path = '/usr/bin/amulecmd'
-token = '1460282614:AAF9GPKkPvhp0BD88BwuaMk_moYQbff9Zg4'
-user_id = 1657793959
-user_txt = 'kutxa'
-aMulepass = 'k69aMuleWeb'
+token = ''
+user_id = 
+user_txt = ''
+aMulepass = ''
 aMulelogo = 'aMuleBNfondo.png'
 avatar = 'avatarBNfondo.png'
-
